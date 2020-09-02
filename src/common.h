cb8f7685cd7657112d53959f71d2e83a439a15a3
Paul Page <pjpage98@gmail.com>
2020-09-01 20:52:30 -0500

    Add common.h

---
+++ src/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+enum Tool {
+    TOOL_PENCIL,
+    TOOL_PAINTBRUSH,
+    TOOL_COLOR_PICKER,
+    TOOL_PAINT_BUCKET,
+    TOOL_SPRAY_CAN,
+
+    FINAL_TOOL_COUNT, // Marks the end of the enum
+};
+
+#endif // COMMON_H
